6eddd669f1a7476cc974bfe95efd45f818c88e8f
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2024-11-06 21:20:19 +0530

    Installed next-themes and added theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       Test
+      <ThemeToggle />
       <Button>Button</Button>
     </div>
   );
