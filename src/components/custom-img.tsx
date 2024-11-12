6171cedae2d29e86f5ee8e72dbff2c2720b24ad3
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2024-11-12 18:35:31 +0530

    Working on shimmer

--- src/components/custom-img.tsx
+++ src/components/custom-img.tsx
@@ -1,6 +1,10 @@
 "use client"
+import shimmer from '@/data/_static/shimmer';
+import toBase64 from '@/data/_static/toBase64';
 import Image from 'next/image'
 import React, { useState } from 'react'
+
+
 
 const CustomImg = (props: { src: string; alt: string }) => {
     const [hasError, setHasError] = useState<boolean>(false)
@@ -11,6 +15,8 @@ const CustomImg = (props: { src: string; alt: string }) => {
             src={hasError ? '/image-placeholder.svg' : props?.src}
             alt={props?.alt}
             fill
+            placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+
             className=' object-cover object-center'
             onError={() => setHasError(true)}
         />
