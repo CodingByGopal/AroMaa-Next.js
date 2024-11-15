2f60f7737c481cbdc5ffe8d739d93ac7b7adbde2
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2024-11-15 13:32:39 +0530

    Meta data progress

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -7,7 +7,17 @@ import { SearchParamsType } from '@/data/_model/searchparams.type';
 import { itemsPerPage } from '@/data/_static/items.per.page';
 import recipeFilters from '@/data/_static/recipe.filters';
 import React, { Suspense } from 'react'
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: 'Recipes',
+    description: "Discover a variety of delicious recipes, search by ingredients, and sort by your favorite filters.",
+    alternates: {
+        canonical: `/recipes`
+    },
+    openGraph: {
+        images: ['/images/recipes-og.png'],
+    },
+}
 
 const Recipes = async (props: {
     searchParams: SearchParamsType
