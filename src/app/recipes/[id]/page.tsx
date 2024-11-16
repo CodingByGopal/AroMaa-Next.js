377716ea9b3faa2a5c39a70c71b0a6ffee63b57d
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2024-11-16 20:04:34 +0530

    Added ISR in recipe details page

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -6,9 +6,12 @@ import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
 import type { Metadata, ResolvingMetadata } from 'next'
 import NoData from '@/components/no-data'
-import { extractId } from '@/data/_helpers/_utils'
+import { extractId, itemsPerPage, slugify } from '@/data/_helpers/_utils'
 import RecipesService from '@/services/recipes.service'
 import { RenderedTimeAgo } from '@/components/rendered-time-ago'
+import { RecipeResponseModel } from '@/data/_model/recipe.response.model'
+
+
 type Params = Promise<{ id: string }>
 
 
@@ -51,6 +54,16 @@ export async function generateMetadata(
 
 
 }
+// generatign static pages for 'limit: itemsPerPage-> Eg. 12'  and revalidating every 60 seconds. Dynamic params are enabled too
+export const revalidate = 60
+export const dynamicParams = true;
+export async function generateStaticParams() {
+    const recipesResponse: RecipeResponseModel = await RecipesService.getAllRecipes({ limit: itemsPerPage, select: "name" })
+    const recipes = recipesResponse.recipes;
+    const ids = recipes.map(recipe => ({ id: `${slugify(recipe?.name)}-${recipe?.id}` }))
+    return ids;
+}
+
 
 const RecipeDetails = async (props: {
     params: Params
@@ -105,10 +118,6 @@ const RecipeDetails = async (props: {
                                             </div> :
                                             null}
                                     </>
-
-
-
-
                                 </div>
                             </div>
                         </div>
