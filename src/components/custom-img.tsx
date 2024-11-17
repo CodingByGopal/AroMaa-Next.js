697b05fff8dd6d1abd76f5a032dfe394bb1959d6
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2024-11-17 17:31:36 +0530

    Fix

--- src/components/custom-img.tsx
+++ src/components/custom-img.tsx
@@ -9,7 +9,6 @@ const CustomImg = (props: { src: string; alt: string }) => {
 
     return (
         <Image
-            sizes='(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw'
             src={hasError ? '/image-placeholder.svg' : props?.src}
             alt={props?.alt}
             fill
